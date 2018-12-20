e2f9a67dd2736d18026039811728fe1b1c0fabf9
Bond <45951465+dev-bond@users.noreply.github.com>
2018-12-19 20:10:50 -0400

    Database functions and config

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
 // Project: Koto
-// Description: Simple API for photo sharing
+// Description: Simple photo sharing platform with an open source REST API
 // Coded by Bond on a cold December night in 2018
 
 // db PDO functions
