05de817bb59d7bb920d394c68451163f81243595
Bond <45951465+dev-bond@users.noreply.github.com>
2018-12-19 20:09:21 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Project: Koto
-// Description: Simple API for photo sharing
+// Description: Simple photo sharing platform with an open source REST API
 // Coded by Bond on a cold December night in 2018
 
 // No configuration settings yet; built to be scalable.
