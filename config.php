c4fe787b8d89185ed605f8069c4ce10de67c9582
Bond <45951465+dev-bond@users.noreply.github.com>
2018-12-19 20:10:03 -0400

    Configuration settings

--- config.php
+++ config.php
@@ -3,7 +3,6 @@
 // Project: Koto
 // Description: Simple photo sharing platform with an open source REST API
 // Coded by Bond on a cold December night in 2018
-
 // No configuration settings yet; built to be scalable.
 
 // Now
